427e39f89d31c4bd592006a0133e7344687f6b51
ngovantin <tindeptry1357@gmail.com>
2025-04-13 22:59:07 +0700

    fix url

--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react'
-import Header from '../components/header.jsx'
+import Header from '../components/Header.jsx'
 import { Outlet } from 'react-router-dom'
 import Loading from '../Loading.jsx'
 
